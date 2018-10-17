ce892786ef82b6192debb3e9f9c4e8572821b7a7
Michael Coughlin <michael.coughlin@ligo.org>
2018-10-17 08:06:03 -0700

    Fix KW_veto_utils.py call

--- copy_db.sh
+++ copy_db.sh
@@ -6,7 +6,10 @@
 #scp /home/eric.coughlin/LineSearch/weeklies/H1/H1_COH_1116633616_1118275216_SHORT_10/results/H1_COH_1116633616_1118275216_SHORT_10.db michael.coughlin@virgo.physics.carleton.edu:/home/michael.coughlin/data
 #scp /home/eric.coughlin/LineSearch/weeklies/H1/H1_COH_1116633616_1118275216_SHORT_100/results/H1_COH_1116633616_1118275216_SHORT_100.db michael.coughlin@virgo.physics.carleton.edu:/home/michael.coughlin/data
 
-scp /home/eric.coughlin/LineSearch/weeklies/H1/H1_COH_1123891217_1124582417_SHORT_1/results/H1_COH_1123891217_1124582417_SHORT_1.db michael.coughlin@virgo.physics.carleton.edu:/home/michael.coughlin/data
-scp /home/eric.coughlin/LineSearch/weeklies/H1/H1_COH_1123891217_1124582417_SHORT_10/results/H1_COH_1123891217_1124582417_SHORT_10.db michael.coughlin@virgo.physics.carleton.edu:/home/michael.coughlin/data
-scp /home/eric.coughlin/LineSearch/weeklies/H1/H1_COH_1123891217_1124582417_SHORT_100/results/H1_COH_1123891217_1124582417_SHORT_100.db michael.coughlin@virgo.physics.carleton.edu:/home/michael.coughlin/data
+#scp /home/eric.coughlin/LineSearch/weeklies/H1/H1_COH_1123891217_1124582417_SHORT_1/results/H1_COH_1123891217_1124582417_SHORT_1.db michael.coughlin@virgo.physics.carleton.edu:/home/michael.coughlin/data
+#scp /home/eric.coughlin/LineSearch/weeklies/H1/H1_COH_1123891217_1124582417_SHORT_10/results/H1_COH_1123891217_1124582417_SHORT_10.db michael.coughlin@virgo.physics.carleton.edu:/home/michael.coughlin/data
+#scp /home/eric.coughlin/LineSearch/weeklies/H1/H1_COH_1123891217_1124582417_SHORT_100/results/H1_COH_1123891217_1124582417_SHORT_100.db michael.coughlin@virgo.physics.carleton.edu:/home/michael.coughlin/data
+
+scp /home/mcoughlin/LineSearch/O2/weeklies/H1/H1_COH_1161388815_1161993615_SHORT_1/results/H1_COH_1161388815_1161993615_SHORT_1.db michael.coughlin@virgo.physics.carleton.edu:/home/michael.coughlin/data
+
 
